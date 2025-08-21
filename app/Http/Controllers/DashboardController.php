2da7651bb373f4f60c4660ef340cb3afa439d802
idn-fahmifn <fahmifn.idn@gmail.com>
2025-08-21 10:27:09 +0700

    4.1

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    // admin dashboard
+    public function admin()
+    {
+        return view('admin.dashboard');
+    }
+
+    public function user()
+    {
+        return view('user.dashboard');
+    }
+}
---
+++ database/migrations/2025_08_21_024119_create_respon_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('respon', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('pengaduan_id')
+            ->constrained('pengaduan')->cascadeOnDelete();
+            $table->text('deskripsi_tanggapan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('respon');
+    }
+};
